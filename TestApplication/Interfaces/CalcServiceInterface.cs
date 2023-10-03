7edac519b3f25299cefe1aea691825018fe4883e
Alexander <an4lex@gmail.com>
2023-10-03 23:14:19 +0300

    Created an interface for calculation methods

---
+++ TestApplication/Interfaces/CalcServiceInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApplication.Interfaces {
+    public interface CalcServiceInterface {
+
+        float Multiply(float a, float b);
+        float Divide(float a, float b);
+        float Sum(float a, float b);
+        float Subtract(float a, float b);
+    }
+}
