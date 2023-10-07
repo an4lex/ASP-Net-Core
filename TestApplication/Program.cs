6c4a704e6642b84a3030de70d4d7ce2a2d6a7241
Alexander <an4lex@gmail.com>
2023-10-08 00:01:33 +0300

    Implemented Routes and Error Handling

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,92 +1,90 @@
-﻿using TestApplication.Interfaces;
-using TestApplication.Services;
+﻿using Microsoft.AspNetCore.Http;
+using TestApplication.Other;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder();
-builder.Services
-    .AddTransient<CalcServiceInterface, CalcService>()
-    .AddTransient<TimeOfTheDayInterface, TimeOfTheDay>();
-
+// Create a web application builder
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-app.MapPost("/calculate", async context => {
-    CalcServiceInterface? calcService = context.RequestServices.GetService<CalcServiceInterface>();
-    var form = await context.Request.ReadFormAsync();
-    var firstValue = int.Parse(form["firstValue"]);
-    var secondValue = int.Parse(form["secondValue"]);
-    var operation = form["operation"];
-    var port = context.Request.Host.Port;
-    float result = 0;
+// Add a JSON configuration file
+builder.Configuration.AddJsonFile("Configurations/library.json");
 
-    switch (operation) {
-        case "+":
-            result = calcService.Sum(firstValue, secondValue);
-            break;
-        case "-":
-            result = calcService.Subtract(firstValue, secondValue);
-            break;
-        case "*":
-            result = calcService.Multiply(firstValue, secondValue);
-            break;
-        case "/":
-            result = calcService.Divide(firstValue, secondValue);
-            break;
+// Map a route for displaying student information by ID
+app.Map("library/students/{id:int?}",
+    async (int? id, HttpContext context, IConfiguration appConfig) => {
+
+        // Retrieve the array of students from the configuration
+        Student[] students = appConfig.GetSection("library:students").Get<Student[]>();
+        StringBuilder sb = new StringBuilder();
+        sb.Append("<div style='font-weight: medium;'>");
+
+        // Check if an ID is provided and within valid bounds
+        if (id.HasValue && students != null && id.Value >= 0 && id.Value < students.Length)
+        {
+            // Display student information if found
+            sb.Append($"<p>Name: {students[id.Value].Name}. ID: {students[id.Value].StudentID}<p>");
+        }
+        else {
+            // Display a message with user information if the ID is not found or invalid
+            sb.Append($"<p>Name: {context.User}. Identity: {context.User.Identity}<p>");
+        }
+
+        sb.Append("</div>");
+        await context.Response.WriteAsync(sb.ToString());
+    });
+
+// Map a route for the library's main page
+app.Map("library", async (HttpContext context, IConfiguration appConfig) =>
+{
+    // Retrieve a greeting message from the configuration
+    string greeting = appConfig.GetSection("greeting").Value;
+    StringBuilder sb = new StringBuilder();
+    sb.Append($"<div style='font-weight: medium;'>{greeting}</div>");
+    await context.Response.WriteAsync(sb.ToString());
+});
+
+// Map a route for displaying a list of books in the library
+app.Map("library/books", async(HttpContext context, IConfiguration appConfig) =>
+{
+    // Retrieve an array of books from the configuration
+    Book[] books = appConfig.GetSection("library:books").Get<Book[]>();
+    StringBuilder sb = new StringBuilder();
+    sb.Append("<div style='font-weight: medium;'>");
+
+    // Iterate through books and display their information
+    foreach (var book in books) {
+        sb.Append($"<p>Book: {book.Title} | Author: {book.Author}<p>");
     }
 
-    context.Response.ContentType = "text/html;charset=utf-8";
-    var responseHtml =
-    $"<h2>Result = {result}</h2>" +
-    $"<a href=https://localhost:{port}>Main Page</a>";
-    context.Response.StatusCode = 200;
-    await context.Response.WriteAsync(responseHtml);
+    sb.Append("</div>");
+    await context.Response.WriteAsync(sb.ToString());
 });
 
-app.MapGet("/", async context =>
+app.Map("/", async (HttpContext context, IConfiguration appConfig) =>
 {
-    var stringBuilder = new StringBuilder();
+    // Retrieve the array of students from the configuration
+    Student[] students = appConfig.GetSection("library:students").Get<Student[]>();
+    StringBuilder sb = new StringBuilder();
+    sb.Append($"<div style='font-weight'>" +
+        $"<ul>" +
+        $"<li><a href='/library'>Library</a></li>" +
+        $"<li><a href='/library/books'>Search for book</a></li>");
 
-    stringBuilder.Append($@"
-    <div>
-        <h1>Calculator</h1>
-        <form method=""post"" action=""/calculate"">
-            <div>
-                <label for=""firstValue"">First value: </label>
-                <input type=""number"" id=""firstValue"" name=""firstValue"" required>
-            </div>
-            <div>
-                <label for=""operation"">Operation type: </label>
-                <select id=""operation"" name=""operation"" required>
-                    <option value=""+"">+</option>
-                    <option value=""-"">-</option>
-                    <option value=""*"">*</option>
-                    <option value=""/"">/</option>
-                </select>
-            </div>
-            <div>
-                <label for=""secondValue"">Second value: </label>
-                <input type=""number"" id=""secondValue"" name=""secondValue"" required>
-            </div>
-            <button type=""submit"">Get result</button>
-        </form>
-    </div>
-");
+    // Generate links to individual student pages
+    for (int i = 0; i < students.Length; i++)
+    {
+        sb.Append($"<li><a href='/library/users/{i}'>{students[i].Name}</a></li>");
+    }
 
-    context.Response.ContentType = "text/html;charset=utf-8";
-    await context.Response.WriteAsync(stringBuilder.ToString());
+    sb.Append($"</ul>" + "</div>");
+    await context.Response.WriteAsync(sb.ToString());
 });
 
-app.MapGet("/time", async context =>
-{
-    TimeOfTheDayInterface? timeOfTheDay = context.RequestServices.GetService<TimeOfTheDayInterface>();
-    string dayAppearance = timeOfTheDay.GetDayTime();
-    string dayTime = timeOfTheDay.GetDayTime();
-    var stringBuilder = new StringBuilder();
-    stringBuilder.Append(
-        $"<h2 style=\"color: {dayAppearance};\">" +
-        $"{dayTime}</h2>");
-    context.Response.ContentType = "text/html;charset=utf-8";
-    context.Response.StatusCode = 200;
-    await context.Response.WriteAsync(stringBuilder.ToString());
-});
+// Use a middleware to handle 404 errors
+app.Use(async (context, next) => {
+    await next.Invoke();
 
+    if (context.Response.StatusCode == 404)
+        await context.Response.WriteAsync("Not Found");
+});
 app.Run();
