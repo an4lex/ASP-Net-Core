e115663bfde78cf181685e8a84b37e65a7cb7926
Alexander <an4lex@gmail.com>
2023-10-08 00:00:23 +0300

    Created a class that contains Student properties.

---
+++ TestApplication/Other/Student.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace TestApplication.Other
+{
+    public class Student
+    {
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+        [JsonProperty("studentID")]
+        public int StudentID { get; set; }
+    }
+}
