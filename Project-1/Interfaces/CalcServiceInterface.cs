d63036d51e6b33e1a2f18736cb6c5637f0411052
an4lex <an4lex@gmail.com>
2023-12-07 20:07:33 +0200

    Initial commit for fifth laboratory.

---
+++ Project-1/Interfaces/CalcServiceInterface.cs
@@ -0,0 +1,13 @@
+﻿namespace Project-1.interfaces
+{
+    public interface ICalcService
+    {
+        float Sum(float a, float b);
+
+        float Subtract(float a, float b);
+
+        float Multiply(float a, float b);
+
+        float Divide(float a, float b);
+    }
+}
