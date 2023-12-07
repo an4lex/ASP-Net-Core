31feb2d6439954ba84241a2c36ecb587228ecfa8
an4lex <an4lex@gmail.com>
2023-12-07 20:28:10 +0200

    Initial commit for 12 laboratory work.

--- lr11/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lr11.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
